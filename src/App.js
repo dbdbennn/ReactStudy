d7b98db0dcd24f38c767d32a5a71c1ec91e908db
Yu Jung Eun <0hh.0553@gmail.com>
2023-04-10 08:51:43 +0900

    1-7

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import Hello from './Hello';
+import Counter from './Counter';
 import Wrapper from './Wrapper';
 
 function App() {
   return (
-    <Wrapper>
-      <Hello name="react" color="red" isSpecial/>
-      <Hello color="pink"/>
-    </Wrapper>
+    <Counter />
   );
 }
 
